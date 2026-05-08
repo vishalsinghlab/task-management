abe1b112997dd63ddc90f87a5c1c616a10f8fadb
vishal-s-gortnm <vishal.s@gortnm.in>
2026-05-09 03:45:15 +0530

    feat: create user schema with role support and password hashing

---
+++ src/models/user.model.js
@@ -0,0 +1,75 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      trim: true,
+      minlength: 3,
+      maxlength: 50,
+    },
+
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 6,
+      select: false,
+    },
+
+    role: {
+      type: String,
+      enum: ["MANAGER", "TEAM_LEAD", "EMPLOYEE"],
+      default: "EMPLOYEE",
+    },
+
+    // Optional hierarchy mapping
+    manager: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+
+    teamLead: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre("save", async function (next) {
+  // Only hash if password modified
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
